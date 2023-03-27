1e19718b9ce1806edc39869e757dcffccd16e51e
hentrademark <hennyboi1176@gmail.com>
2023-03-28 00:32:34 +0100

    Added Chapter 8

---
+++ Chapters/Chapter 8/Program8_1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace Program8_1 {
+    class Program {
+        static Random _rand = new Random();
+        static int _com;
+        static int _pla;
+        static bool Menu() {
+            Console.WriteLine("There is a game where you and the computer roll dice and whoever gets to 30 first wins");
+            string ans = "";
+            do {
+                Console.WriteLine("Do you want to play?");
+                Console.Write("(Y or N): ");
+                ans = Console.ReadLine().ToUpper();
+                if (!Regex.IsMatch(ans,"^(Y|N)$")) {
+                    Console.WriteLine("\nInput not correct. Please enter \"Y\" or \"N\"\n");
+                }
+            } while (!Regex.IsMatch(ans,"^(Y|N)$"));
+
+            if (ans == "Y") { PlayGame(); }
+
+            return ans == "Y";
+        }
+        static void PlayGame() {
+            int num;
+            _com = 0;
+            _pla = 0;
+            if (_rand.Next(2) == 0) {
+                Console.WriteLine("Computer goes first.\n");
+                Console.Write("\nComputer is rolling now");
+                for (int i = 0; i < 3; i++) {
+                    Thread.Sleep(250); Console.Write(".");
+                }
+                num = RollDice();
+                Console.WriteLine("\n\nComputer rolled a {0}",num);
+                _com += num;
+            }
+            else { Console.WriteLine("You go first.\n"); }
+
+            while (_com < 30 && _pla < 30) {
+                Console.WriteLine("Scores: Player - {0}, Computer - {1}",_pla,_com);
+                Console.WriteLine("Your turn!");
+                Console.Write("Press ENTER to roll the dice");
+                Console.ReadLine();
+                num = RollDice();
+                Console.WriteLine("You rolled a {0}!",num);
+                _pla += num;
+                if (_pla < 30) {
+                    Console.WriteLine("Scores: Player - {0}, Computer - {1}",_pla,_com);
+                    Console.Write("\nComputer is rolling now");
+                    for (int i = 0; i < 3; i++) {
+                        Thread.Sleep(250); Console.Write("."); }
+                    num = RollDice();
+                    Console.WriteLine("\n\nComputer rolled a {0}",num);
+                    _com += num;
+                }
+            }
+            string whowon = _com >= 30 ? "Computer won..." : "Player won!";
+            Console.WriteLine(whowon);
+        }
+        static int RollDice() {
+            return _rand.Next(1,7);
+        }
+        static void Main(string[] args) {
+            bool play = Menu();
+            if (play) {
+                bool again = true;
+                do {
+                    string ans = "";
+                    do {
+                        Console.WriteLine("Do you want to play again?");
+                        Console.Write("(Y or N): ");
+                        ans = Console.ReadLine().ToUpper();
+                        if (!Regex.IsMatch(ans,"^(Y|N)$")) {
+                            Console.WriteLine("\nInput not correct. Please enter \"Y\" or \"N\"\n");
+                        }
+                    } while (!Regex.IsMatch(ans,"^(Y|N)$"));
+                    again = ans == "Y";
+                    if (again) { PlayGame(); }
+                } while (again);
+                Console.WriteLine("\nOkay, Good game!"); 
+            } else { Console.WriteLine("Boo you're boring"); }
+        }
+    }
+}
---
+++ Chapters/Chapter 8/Program8_2/Program.cs
@@ -0,0 +1,110 @@
+﻿// I didn't like the way that the original table looked so I made a new one
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace Program8_2 {
+    class Program {
+        static void Main(string[] args) {
+            //  Defines the 2 boards. "board" -> background board, "Board" -> visible board
+            char[,] board = new char[10, 10], Board = new char[10, 10];
+            // Defines the point counter
+            int points = 0, row = 0, col = 0;
+            // Gives both boards appropriate values to play the game
+            MakeBoard(board, Board);
+            // Repeats 10 times or until all 6 treasure plots have been uncovered
+            for (int i = 0; i < 10 && points < 60; i++) {
+                // Prints the board
+                PrintBoard(Board);
+                // Gets valid plot coordinates and stores them in the array
+                Validation(ref row, ref col);
+                // With the information gotten, calculates whether the user gets points or not
+                CalculatePoints(row, col, board, Board, ref points, ref i);
+            }
+            // Prints the board
+            PrintBoard(Board);
+            // Validates whether the user won or lost, then writes the points they got
+            string writeline = points == 60 ? "\nYou won!" : "\nYou lost...";
+            Console.WriteLine(writeline);
+            Console.WriteLine("You got {0} points",points);
+        }
+
+        static void CalculatePoints(int row, int col, char[,] board, char[,] Board, ref int points, ref int i) {
+            // If the user already picked the plot before
+            if (Board[row, col] != ' ') {
+                Console.WriteLine("You've already picked that plot! ");
+                // Gives a go back to the user, but keeps the points the same
+                i--;
+            }
+            // If the user picks a plot with treasure
+            else if (board[row, col] == 'T') {
+                Console.WriteLine("You found treasure! Added 10 points");
+                // Update the visible board
+                Board[row, col] = 'T';
+                // Give the user 10 points
+                points += 10;
+            }
+            else{
+                // If anything else happens (i.e. Picked a new plot without treasure)
+                Console.WriteLine("Missed the treasure...");
+                // Update visible board
+                Board[row, col] = 'X';
+                // No points, no retry
+            }
+        }
+
+        static void Validation(ref int row, ref int col) {
+            // Defines a row and col placeholder string for inputs
+            string strrow = "", strcol = "";
+            do {
+                // Takes inputs for row and col
+                Console.Write("Enter a row number: ");
+                strrow = Console.ReadLine();
+                Console.Write("Enter a column number: ");
+                strcol = Console.ReadLine();
+                Console.WriteLine();
+                // If either of them are not in the right format (any integer from 0 to 9), tells them that
+                if (!Regex.IsMatch(strrow, "^[0-9]\\z") || !Regex.IsMatch(strcol, "^[0-9]\\z")) {
+                    Console.WriteLine("Invalid input detected");
+                    Console.WriteLine("Please enter a integer from 0 to 9\n");
+                }
+            } while (!Regex.IsMatch(strrow, "^[0-9]\\z") || !Regex.IsMatch(strcol, "^[0-9]\\z"));
+            // sets the row and col to the valid inputs
+            row = int.Parse(strrow);
+            col = int.Parse(strcol);
+        }
+
+        static void MakeBoard(char[,] board, char[,] Board) {
+            // Sets every character in both "board" and "Board" to ' '
+            for (int a = 0; a < 10; a++) {
+                for (int b = 0; b < 10; b++) {
+                    board[a,b] = ' ';
+                    Board[a,b] = ' ';
+                }
+            }
+            // For "board", places 'T' in 6 random places
+            Random r = new Random();
+            int row = 0, col = 0;
+            for (int i = 0; i < 6; i++) {
+                row = r.Next(10);
+                col = r.Next(10);
+                if (board[row, col] == ' ') { board[row, col] = 'T'; }
+                else { i--; }
+            }
+            
+        }
+
+        static void PrintBoard(char[,] board) {
+            // Builds the board
+            Console.WriteLine("  || 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |");
+            Console.WriteLine("============================================");
+            for (int i = 0; i < 10; i++) {
+                Console.Write("{0} ||",i);
+                for (int j = 0; j < 10; j++) {
+                    Console.Write(" {0} |",board[i,j]);
+                }
+                Console.WriteLine("\n--------------------------------------------");
+            }
+        }
+    }
+}
